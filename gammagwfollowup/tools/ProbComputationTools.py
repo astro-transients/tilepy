46679d89b795c30e0ad63d32cac26e8177c4e6f4
mseglar <monica.seglar-arroyo@cea.fr>
2022-08-03 14:56:57 +0200

    Rearrange the HESS tools so that they are generalized in the folder tools. Those only used by developers went to PointingPlotting, e.g. to visualize results of CTA Simulations
    Former-commit-id: 2a1ac5abd5d1d47269ca4252ea0b9467a8a75b33
    Former-commit-id: d688b1175f8709f1e2c824a5229efaa1ac3ba349

---
+++ gammagwfollowup/tools/ProbComputationTools.py
@@ -0,0 +1,38 @@
+from gammagwfollowup.include.PointingTools import (LoadHealpixMap,CorrelateGalaxies_LVC,SubstractPointings2D,
+                                  SubstractPointings, LoadGalaxies)
+
+import healpy as hp
+
+
+def ComputeCoveredProbability(GWFile, PointingsFile, galFile="../dataset/GLADE.txt", FOV = 2.0, MinimumProbCutForCatalogue = 0.01):
+
+    print("===========================================================================================")
+    print("Starting the computation of covered probability from the following files\n")
+    print('Loading map from ', GWFile)
+    print("Filename : ",PointingsFile)
+    print("===========================================================================================")
+    
+    
+    prob, distmu, distsigma, distnorm, detectors, fits_id, thisDistance, thisDistanceErr = LoadHealpixMap(GWFile)
+    npix = len(prob)
+    nside = hp.npix2nside(npix)
+    
+    cat = LoadGalaxies(galFile)
+    
+    has3D = True
+    if (len(distnorm) == 0):
+        print("Found a generic map without 3D information")
+        # flag the event for special treatment
+        has3D = False
+    else:
+        print("Found a 3D reconstruction")
+    
+    alreadysumipixarray = []
+
+    
+    tGals0, sum_dP_dV = CorrelateGalaxies_LVC(prob, distmu, distsigma, distnorm, cat, has3D, MinimumProbCutForCatalogue)
+    #alreadysumipixarray,AlreadyObservedPgw = SubstractPointings2D(PointingsFile,prob, nside, FOV,alreadysumipixarray)
+    ra, dec, tGals, alreadyObservedPgw, alreadyObservedPgal, alreadysumipixarray = SubstractPointings(PointingsFile, tGals0, alreadysumipixarray, sum_dP_dV, FOV, prob,nside)
+    
+    print('Total PGW covered: ', sum(alreadyObservedPgw))
+    print('Total PGAL covered: ',sum(alreadyObservedPgal))
