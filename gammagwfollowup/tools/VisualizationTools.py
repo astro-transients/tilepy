81f85458a49478b633c789952416ad2ebed1cae5
mseglar <monica.seglar-arroyo@cea.fr>
2022-08-03 14:56:57 +0200

    Rearrange the HESS tools so that they are generalized in the folder tools. Those only used by developers went to PointingPlotting, e.g. to visualize results of CTA Simulations
    Former-commit-id: 46679d89b795c30e0ad63d32cac26e8177c4e6f4 [formerly d688b1175f8709f1e2c824a5229efaa1ac3ba349] [formerly 2a1ac5abd5d1d47269ca4252ea0b9467a8a75b33]
    Former-commit-id: 4a452aace358ac55c6a25785e95225f55bbf2b11
    Former-commit-id: 406e829eab177a4e7f5a315a4fdcc6d36509d88c

---
+++ gammagwfollowup/tools/VisualizationTools.py
@@ -0,0 +1,207 @@
+import os
+import sys
+from gammagwfollowup.include.PointingTools import NightDarkObservation, NightDarkObservationwithGreyTime, LoadHealpixMap
+from gammagwfollowup.include.Observatories import CTANorthObservatory, CTASouthObservatory, HESSObservatory
+from gammagwfollowup.include.PointingTools import getdate
+from gammagwfollowup.include.PointingPlotting import LoadPointingsGAL, PlotPointingsTogether
+from gammagwfollowup.include.PointingTools import Get90RegionPixReduced, TransformRADec
+
+import healpy as hp
+import numpy as np
+from astropy.coordinates import SkyCoord
+import datetime
+from datetime import timezone
+import matplotlib.pyplot as plt
+from astropy import units as u
+import astropy.coordinates as co
+
+from astropy.utils import iers
+
+# from astropy.utils.iers import conf as iers_conf
+# iers_conf.iers_auto_url = 'ftp://cddis.gsfc.nasa.gov/pub/products/iers/finals2000A.all'
+# iers_conf.iers_auto_url_mirror = 'ftp://cddis.gsfc.nasa.gov/pub/products/iers/finals2000A.all'
+iers_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../dataset/finals2000A.all')
+iers.IERS.iers_table = iers.IERS_A.open(iers_file)
+
+
+def LocateSource(filename, ra, dec, PercentCov=90):
+    
+    prob, distmu, distsigma, distnorm, detectors, fits_id, thisDistance, thisDistanceErr = LoadHealpixMap(filename)
+    npix = len(prob)
+    nside = hp.npix2nside(npix)
+    
+    pix_ra1, pix_dec1, area = Get90RegionPixReduced(prob, PercentCov, nside)
+    
+    reduced_nside = 512
+    coordinates = TransformRADec(ra, dec)
+    ra1 = coordinates.ra.rad
+    dec1 = coordinates.dec.rad
+    
+    radecs = co.SkyCoord(pix_ra1, pix_dec1, frame='fk5', unit=(u.deg, u.deg))
+    pix_ra11 = radecs.ra.rad
+    pix_dec11 = radecs.dec.rad
+    
+    Igrb = hp.ang2pix(reduced_nside, ra1, dec1, lonlat=True)
+    Imap = hp.ang2pix(reduced_nside, pix_ra11, pix_dec11, lonlat=True)
+    
+    print(np.isin(Igrb, Imap))
+    
+    hp.mollview(prob, title="Locate source")
+    hp.visufunc.projplot(coordinates.ra, coordinates.dec, 'r.', lonlat=True)
+    hp.graticule()
+    plt.show()
+
+
+def VisibilityOverview_forZenithCut(GWFile, date=datetime.datetime.now(timezone.utc), zenithcut=60):
+    time = getdate(date)
+    
+    print("===========================================================================================")
+    
+    print('Loading map from ', GWFile)
+    print("Input time: ", time)
+    print("Zenith angle cut for plots: ", zenithcut)
+    
+    print("===========================================================================================")
+    
+    #################################  Other parameters ##############################
+    
+    observatory = HESSObservatory()
+    observatory2 = CTANorthObservatory()
+    
+    prob, distmu, distsigma, distnorm, detectors, fits_id, thisDistance, thisDistanceErr = LoadHealpixMap(GWFile)
+    
+    TITLE_HESS = observatory.Name + '(red) &' + observatory2.Name + '(blue) visibility at ' + str(time)
+
+    # Just do a plotting
+    hp.mollview(prob, coord='C', title=TITLE_HESS)  # Celestial=Equatorial
+    # frame = co.AltAz(obstime=time, location=observatory.Location)
+    
+    altcoord = np.empty(4000)
+    altcoord.fill(90 - zenithcut)
+    azcoord = np.random.rand(4000) * 360
+    
+    RandomCoord = SkyCoord(azcoord, altcoord, frame='altaz', unit=(u.deg, u.deg), obstime=time,
+                           location=observatory.Location)
+    RandomCoord_radec = RandomCoord.transform_to('fk5')
+    hp.visufunc.projplot(RandomCoord_radec.ra, RandomCoord_radec.dec, 'red', lonlat=True, coord="C", linewidth=0.4)
+    
+    RandomCoord = SkyCoord(azcoord, altcoord, frame='altaz', unit=(u.deg, u.deg), obstime=time,
+                           location=observatory2.Location)
+    RandomCoord_radec = RandomCoord.transform_to('fk5')
+    hp.visufunc.projplot(RandomCoord_radec.ra, RandomCoord_radec.dec, 'blue', lonlat=True, coord="C", linewidth=0.4)
+    
+    hp.graticule()
+    plt.savefig("Visibility_Overview.png")
+    print("-----------------------------------------------------------------")
+    print('Observatory 1 is : ', observatory.Name, "and observatory 2 is: ", observatory2.Name)
+    print("\n plot created: Visibility_Overview.png")
+
+
+def Time_DarkTime_GreyTime(GWFile, date=datetime.datetime.now(timezone.utc), zenithcut=60, maxNights=1, maxDuration=28,
+                           minDuration=10):
+    time = getdate(date)
+    
+    #################################  Other parameters ##############################
+    
+    observatory = HESSObservatory()
+    
+    prob, distmu, distsigma, distnorm, detectors, fits_id, thisDistance, thisDistanceErr = LoadHealpixMap(GWFile)
+    npix = len(prob)
+    nside = hp.npix2nside(npix)
+    
+    altcoord = np.empty(4000)
+    altcoord.fill(90 - zenithcut)
+    azcoord = np.random.rand(4000) * 360
+    
+    NightDarkRuns = NightDarkObservation(time, observatory, maxNights, maxDuration, minDuration)
+    NightGreyRuns = NightDarkObservationwithGreyTime(time, observatory, maxNights, maxDuration, minDuration)
+    
+    print("-----------------------------------------------------------------")
+    print('Observatory is: ', observatory.Name)
+    print('Start of Darkness:', NightDarkRuns[0])
+    print(NightDarkRuns)
+    print('Last run:', NightDarkRuns[-1])
+    print('Total number of runs fulfilling darkness condition:', len(NightDarkRuns))
+    print("-----------------------------------------------------------------")
+    
+    print('Start of Greyness:', NightGreyRuns[0])
+    print(NightGreyRuns)
+    print('Last run:', NightGreyRuns[-1])
+    print('Total number of runs fulfilling greyness condition:', len(NightGreyRuns))
+    print("-----------------------------------------------------------------")
+
+    # InputTime
+    RandomCoord = SkyCoord(azcoord, altcoord, frame='altaz', unit=(u.deg, u.deg), obstime=time,
+                           location=observatory.Location)
+    RandomCoord_radec = RandomCoord.transform_to('fk5')
+    hp.visufunc.projplot(RandomCoord_radec.ra, RandomCoord_radec.dec, 'red', lonlat=True, coord="C", linewidth=0.4)
+    
+    # NightDarkRuns[0]
+    RandomCoord = SkyCoord(azcoord, altcoord, frame='altaz', unit=(u.deg, u.deg), obstime=NightDarkRuns[0],
+                           location=observatory.Location)
+    RandomCoord_radec = RandomCoord.transform_to('fk5')
+    hp.visufunc.projplot(RandomCoord_radec.ra, RandomCoord_radec.dec, 'orange', lonlat=True, coord="C", linewidth=0.4)
+
+    # NightDarkRuns[-1]
+    RandomCoord = SkyCoord(azcoord, altcoord, frame='altaz', unit=(u.deg, u.deg), obstime=NightDarkRuns[-1],
+                           location=observatory.Location)
+    RandomCoord_radec = RandomCoord.transform_to('fk5')
+    hp.visufunc.projplot(RandomCoord_radec.ra, RandomCoord_radec.dec, 'orange', lonlat=True, coord="C", linewidth=0.4)
+
+    # NightGreyRuns[0]
+    RandomCoord = SkyCoord(azcoord, altcoord, frame='altaz', unit=(u.deg, u.deg), obstime=NightGreyRuns[0],
+                           location=observatory.Location)
+    RandomCoord_radec = RandomCoord.transform_to('fk5')
+    hp.visufunc.projplot(RandomCoord_radec.ra, RandomCoord_radec.dec, 'blue', lonlat=True, coord="C", linewidth=0.4)
+
+    # NightGreyRuns[-1]
+    RandomCoord = SkyCoord(azcoord, altcoord, frame='altaz', unit=(u.deg, u.deg), obstime=NightGreyRuns[-1],
+                           location=observatory.Location)
+    RandomCoord_radec = RandomCoord.transform_to('fk5')
+    hp.visufunc.projplot(RandomCoord_radec.ra, RandomCoord_radec.dec, 'blue', lonlat=True, coord="C", linewidth=0.4)
+    hp.graticule()
+    plt.savefig("VisibilityTime_DarkTime_GreyTime_Overview" + observatory.Name + '.png')
+    
+    print('Plot created for observatory : ', observatory.Name)
+    print("-----------------------------------------------------------------")
+
+
+def CompareTwoTilings(GWFile, PointingsFile1=False, PointingsFile2=False, FOV=2):
+    # Format of Pointing Files should be YYYY-MM-DD hh:mm:ss RAarray DECarray P_GWarray P_Galarray Round)
+    
+    print("===========================================================================================")
+    print("Starting the pointing plotting from the following files\n")
+    
+    print('Loading map from ', GWFile)
+    print("Filename 1: ", PointingsFile1)
+    print("Filename 2: ", PointingsFile2)
+    
+    prob, distmu, distsigma, distnorm, detectors, fits_id, thisDistance, thisDistanceErr = LoadHealpixMap(GWFile)
+    npix = len(prob)
+    nside = hp.npix2nside(npix)
+    
+    if (PointingsFile1 == 'False') or (PointingsFile2 == 'False'):
+        print('At least one of the pointings has not been given, try again')
+        # Just do a plotting
+        hp.mollview(prob, coord='C')
+        hp.graticule()
+        plt.show()
+    
+    else:
+        print('Loading pointings')
+        
+        ObservationTimearray1, Coordinates1, Pgw1, Pgal1 = LoadPointingsGAL(PointingsFile1)
+        ObservationTimearray2, Coordinates2, Pgw2, Pgal2 = LoadPointingsGAL(PointingsFile2)
+        
+        print('Summary of 1st file: sum(PW)=', sum(Pgw1), 'sum(PGAL)=', sum(Pgal1), 'total pointings', len(Pgal1))
+        print('Summary of 2nd file: sum(PW)=', sum(Pgw2), 'sum(PGAL)=', sum(Pgal1), 'total pointings', len(Pgal2))
+        print("===========================================================================================")
+        
+        name1 = 'File1'
+        name2 = 'File2'
+        
+        PlotPointingsTogether(prob, ObservationTimearray1, Coordinates1, name1, Coordinates2, name2, nside, FOV,
+                              doplot=True)
+    plt.show()
+
+
