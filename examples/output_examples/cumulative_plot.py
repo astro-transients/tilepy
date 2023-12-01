4b5bdca42cc2f3139dc0a3924d48c56c9fa23e68
Halim Ashkar <halim.ashkar@cea.fr>
2023-12-01 18:37:18 +0100

    Adding all plots from the paper and the changes to the code to make them
    Former-commit-id: 43369e6d29960adea6bd16ea6b1d2bafb96b4592
    Former-commit-id: 2a5baa344a70b44fd9fda229a16434d056759ffc

---
+++ examples/output_examples/cumulative_plot.py
@@ -0,0 +1,3 @@
+from tilepy.tools.PlotCumulativeCoverage import ObtainCumulativeProbabilityPlot
+
+ObtainCumulativeProbabilityPlot('S190814bv/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'Pgal')
