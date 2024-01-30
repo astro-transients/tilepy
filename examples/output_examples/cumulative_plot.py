02058e8269a743eaa85544496067dd7381e7574d
Monica Seglar <monica.seglar-arroyo@cea.fr>
2024-01-30 16:39:59 +0100

    Plot Cumulative Coverage clips repeating times to show in the plot

--- examples/output_examples/cumulative_plot.py
+++ examples/output_examples/cumulative_plot.py
@@ -1,3 +1,8 @@
 from tilepy.tools.PlotCumulativeCoverage import ObtainCumulativeProbabilityPlot
 
-ObtainCumulativeProbabilityPlot('S190814bv/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'Pgal')
+#ObtainCumulativeProbabilityPlot('S190814bv/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'Pgal')
+ObtainCumulativeProbabilityPlot('bn231012231_PAPER.fit/PGWinFoV_NObs/SuggestedPointings_GWOptimisation.txt','GRB 231012A', 'PGW')
+ObtainCumulativeProbabilityPlot('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'Pgal')
+ObtainCumulativeProbabilityPlot('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'PGW')
+ObtainCumulativeProbabilityPlot('MS230826n_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S230826n', 'Pgal')
+ObtainCumulativeProbabilityPlot('MS230826n_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S230826n', 'PGW')
--- tilepy/tools/PlotCumulativeCoverage.py
+++ tilepy/tools/PlotCumulativeCoverage.py
@@ -25,27 +25,41 @@ def ObtainCumulativeProbabilityPlot(folder_path, event_name, WhatToPlot):
     df = pd.read_csv(file_path, sep=' ')  # Adjust the separator if needed
 
     observatory_column = df.columns[-1]
+    
     grouped_data = df.groupby(observatory_column)
 
-    x_values2 = pd.to_datetime(df["Observation Time UTC"])
-    y_values2 = df[WhatToPlot].cumsum()  # Cumulative sum of PGal values
+    # Find the row with the maximum value in 'value_column' for each group in 'group_column'
+    df['cumsum'] = df[WhatToPlot].cumsum()  # Cumulative sum of PGal values
 
+    max_value_indices = df.groupby("Observation Time UTC")['cumsum'].idxmax()
+
+    # Select the rows with the maximum values
+    max_rows = df.loc[max_value_indices]
+
+    # Display the resulting DataFrame
+    print(max_rows)
+    print('---------------')
+    print('---------------')
+    print(df[WhatToPlot])
+    print('---------------')
+    print('---------------')
+    x_values2 = pd.to_datetime(max_rows["Observation Time UTC"])
+    y_values2 = max_rows["cumsum"]
     # Plot the cumulative values for all observatories
-    plt.plot(x_values2, y_values2, label='All Observatories (Cumulative)', color='black')
+    plt.plot(x_values2, y_values2, label='All Observatories', color='black')
+    
 
     for observatory, observatory_data in grouped_data:
-        print(f"Observatory: {observatory}")
-        print(observatory_data)
+        #print(f"Observatory: {observatory}")
+        #print(observatory_data)
         print("\n")
         # Extract relevant columns for the plot
         x_values = pd.to_datetime(observatory_data["Observation Time UTC"])
-        print(x_values)
+        #print(x_values)
         y_values = observatory_data[WhatToPlot].cumsum()
 
         # Plot the values for each observatory
-        plt.plot(x_values, y_values, label=observatory)
-
-
+        plt.plot(x_values, y_values,label=observatory)
 
     # Add labels and legend
     # Format the x-axis to display date and time
@@ -64,9 +78,8 @@ def ObtainCumulativeProbabilityPlot(folder_path, event_name, WhatToPlot):
     #plt.yscale('log')  # Set Y-axis to log scale
     plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
 
-
     plt.legend()
     plt.grid()
-
-    plt.savefig('%s_cumulativeDistribution.png' %(event_name), dpi=300, bbox_inches='tight')
-    plt.show()
+    plt.savefig('%s_%s_cumulativeDistribution.png' %(event_name,WhatToPlot), dpi=300, bbox_inches='tight')
+    plt.close()
+    #plt.show()
