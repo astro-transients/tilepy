55659e4e9f0fe8786ee5296b73caf1a09ed2137b
Monica Seglar <monica.seglar-arroyo@cea.fr>
2024-01-30 17:35:12 +0100

    Added function that creates log plot and in x axis is time after event
    Former-commit-id: 7b29309cf46c7868874917fcb261aad27a4d3730
    Former-commit-id: 3d7353b546c1f873dcb4391a3fc3b085ed78e185

--- examples/output_examples/cumulative_plot.py
+++ examples/output_examples/cumulative_plot.py
@@ -1,8 +1,8 @@
-from tilepy.tools.PlotCumulativeCoverage import ObtainCumulativeProbabilityPlot
+from tilepy.tools.PlotCumulativeCoverage import ObtainCumulativeProbabilityPlot, ObtainCumulativeProbabilityPlotLog
 
 #ObtainCumulativeProbabilityPlot('S190814bv/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'Pgal')
 ObtainCumulativeProbabilityPlot('bn231012231_PAPER.fit/PGWinFoV_NObs/SuggestedPointings_GWOptimisation.txt','GRB 231012A', 'PGW')
-ObtainCumulativeProbabilityPlot('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'Pgal')
-ObtainCumulativeProbabilityPlot('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'PGW')
+ObtainCumulativeProbabilityPlotLog('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'Pgal','2023-09-15 01:30:10')
+ObtainCumulativeProbabilityPlotLog('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'PGW','2023-09-15 01:30:10')
 ObtainCumulativeProbabilityPlot('MS230826n_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S230826n', 'Pgal')
 ObtainCumulativeProbabilityPlot('MS230826n_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S230826n', 'PGW')
