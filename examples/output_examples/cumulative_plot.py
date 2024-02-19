f4933c4d7050581c03d7b18843349437309df676
Monica Seglar <monica.seglar-arroyo@cea.fr>
2024-02-19 10:00:56 +0100

    Added functions specifically for the plots of the paper

--- examples/output_examples/cumulative_plot.py
+++ examples/output_examples/cumulative_plot.py
@@ -1,13 +1,13 @@
-from tilepy.tools.PlotCumulativeCoverage import ObtainCumulativeProbabilityPlot,ObtainCumulativeProbabilityPlotCTA, ObtainCumulativeProbabilityPlotLog
+from tilepy.tools.PlotCumulativeCoverage import ObtainCumulativeProbabilityPlotLST,ObtainCumulativeProbabilityPlotMWL,ObtainCumulativeProbabilityPlotCTA, ObtainCumulativeProbabilityPlotLog
 
 #ObtainCumulativeProbabilityPlot('S190814bv/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'Pgal')
-ObtainCumulativeProbabilityPlot('bn231012231_PAPER.fit/PGWinFoV_NObs/SuggestedPointings_GWOptimisation.txt','GRB 231012A', 'PGW')
-#ObtainCumulativeProbabilityPlotLog('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation_noHA.txt','S190814bv', 'Pgal','2023-09-15 01:30:10')
-#ObtainCumulativeProbabilityPlotLog('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation_noHA.txt','S190814bv', 'PGW','2023-09-15 01:30:10')
-ObtainCumulativeProbabilityPlot('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation_noHA.txt','S190814bv', 'Pgal')
-ObtainCumulativeProbabilityPlot('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation_noHA.txt','S190814bv', 'PGW')
+#ObtainCumulativeProbabilityPlotLST('bn231012231_PAPER.fit/PGWinFoV_NObs/SuggestedPointings_GWOptimisation.txt','GRB 231012A', 'PGW')
+##ObtainCumulativeProbabilityPlotLog('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation_noHA.txt','S190814bv', 'Pgal','2023-09-15 01:30:10')
+##ObtainCumulativeProbabilityPlotLog('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation_noHA.txt','S190814bv', 'PGW','2023-09-15 01:30:10')
+ObtainCumulativeProbabilityPlotMWL('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation_noHA.txt','S190814bv', 'Pgal')
+ObtainCumulativeProbabilityPlotMWL('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation_noHA.txt','S190814bv', 'PGW')
 
-#ObtainCumulativeProbabilityPlot('MS230826n_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S230826n', 'Pgal')
-#ObtainCumulativeProbabilityPlot('MS230826n_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S230826n', 'PGW')3
-ObtainCumulativeProbabilityPlotCTA('927563_lalinference_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','927563', 'PGW')
-ObtainCumulativeProbabilityPlotCTA('927563_lalinference_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','927563', 'Pgal')
+##ObtainCumulativeProbabilityPlot('MS230826n_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S230826n', 'Pgal')
+##ObtainCumulativeProbabilityPlot('MS230826n_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S230826n', 'PGW')3
+#ObtainCumulativeProbabilityPlotCTA('927563_lalinference_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','927563', 'PGW')
+#ObtainCumulativeProbabilityPlotCTA('927563_lalinference_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','927563', 'Pgal')
--- tilepy/tools/PlotCumulativeCoverage.py
+++ tilepy/tools/PlotCumulativeCoverage.py
@@ -48,7 +48,7 @@ def ObtainCumulativeProbabilityPlot(folder_path, event_name, WhatToPlot):
     
     y_values2 = max_rows["cumsum"]
     # Plot the cumulative values for all observatories
-    plt.plot(max_rows['date_column'], y_values2, label='LST1-4', color='black')
+    plt.plot(max_rows['date_column'], y_values2, label='All Observatories', color='black')
     
 
     for observatory, observatory_data in grouped_data:
@@ -74,6 +74,221 @@ def ObtainCumulativeProbabilityPlot(folder_path, event_name, WhatToPlot):
 
     #plt.tight_layout()  # Adjust layout for better spacing
     plt.title("%s trigger coverage on 2023-10-12" %(event_name))
+    plt.xlabel('Date [UTC]')
+    plt.ylabel('Total Covered Probability [%]')
+    #plt.xscale('log')  # Set X-axis to log scale
+    #plt.yscale('log')  # Set Y-axis to log scale
+    plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+
+    plt.legend()
+    plt.grid()
+    plt.savefig('%s_%s_cumulativeDistribution.png' %(event_name,WhatToPlot), dpi=300, bbox_inches='tight')
+    plt.close()
+    #plt.show()
+
+
+def ObtainCumulativeProbabilityPlotMWL(folder_path, event_name, WhatToPlot):
+
+    # Get a list of all .txt files in the specified folder
+    
+    #file_list = glob.glob(folder_path)
+    #print(folder_path)
+
+    # Initialize an empty DataFrame to store the cumulative 5th columns
+    file_path =  folder_path 
+    cumulative_column = None
+
+    # Initialize a list to store individual 5th columns for plotting
+    individual_columns = []
+
+    # Loop through each file and add the 5th column to the cumulative DataFrame
+    # Loop through each file and add the 5th column to the cumulative DataFrame
+    # Read the .txt file into a Pandas DataFrame
+    df = pd.read_csv(file_path, sep=' ')  # Adjust the separator if needed
+
+    observatory_column = df.columns[-1]
+    df[WhatToPlot] = df[WhatToPlot]*100
+    grouped_data = df.groupby(observatory_column)
+
+    # Find the row with the maximum value in 'value_column' for each group in 'group_column'
+    df['cumsum'] = df[WhatToPlot].cumsum()  # Cumulative sum of PGal values
+
+    max_value_indices = df.groupby("Observation Time UTC")['cumsum'].idxmax()
+
+    # Select the rows with the maximum values
+    max_rows = df.loc[max_value_indices]
+
+    # Display the resulting DataFrame
+    print(max_rows)
+    print('---------------')
+    print('---------------')
+    print(df[WhatToPlot])
+    print('---------------')
+    print('---------------')
+    #x_values2 = pd.to_datetime(max_rows["Observation Time UTC"])
+
+    max_rows['date_column'] = pd.to_datetime(max_rows["Observation Time UTC"])+pd.Timedelta(minutes=15)
+
+    y_values2 = max_rows["cumsum"]
+    # Plot the cumulative values for all observatories
+    plt.plot(max_rows['date_column'], y_values2, '+',label='All Observatories', color='black')
+    
+
+    for observatory, observatory_data in grouped_data:
+        print(f"Observatory: {observatory}")
+        print(observatory_data)
+        print("\n")
+        # Extract relevant columns for the plot
+        x_values = pd.to_datetime(observatory_data["Observation Time UTC"])
+        #print(x_values)
+        y_values = observatory_data[WhatToPlot].cumsum()
+
+        # Plot the values for each observatory
+        y_values_shifted= [0] + y_values.tolist()[:-1]
+
+        # Remove the last element from the column
+        print(y_values)
+        print(y_values_shifted)
+        print(observatory)
+        if(observatory=='ESO'):
+            x_shifted=x_values+pd.Timedelta(minutes=5)
+            plt.plot(x_values, y_values_shifted,'.',color = 'darkorange', label=observatory)
+            plt.plot(x_shifted, y_values,'.', color = 'darkorange')
+        if(observatory=='ESO2'):
+            x_shifted=x_values+pd.Timedelta(minutes=5)
+            plt.plot(x_values, y_values_shifted,'.',color = 'brown',label=observatory)
+            plt.plot(x_shifted, y_values,'.',color = 'brown')
+        if(observatory=='LP'):
+            x_shifted=x_values+pd.Timedelta(minutes=5)
+            plt.plot(x_values, y_values_shifted,'.',color = 'blueviolet',label=observatory)
+            plt.plot(x_shifted, y_values,'.',color = 'blueviolet')    
+        if(observatory=='LPL'):
+            x_shifted=x_values+pd.Timedelta(minutes=5)
+            plt.plot(x_values, y_values_shifted,'.',color = 'yellowgreen',label=observatory)
+            plt.plot(x_shifted, y_values,'.',color = 'yellowgreen')   
+        if(observatory=='OHP'):
+            x_shifted=x_values+pd.Timedelta(minutes=15)
+            plt.plot(x_values, y_values_shifted,'.',color = 'red',label=observatory)
+            plt.plot(x_shifted, y_values,'.',color = 'red')   
+        if(observatory=='SA'):
+            x_shifted=x_values+pd.Timedelta(minutes=5)
+            plt.plot(x_values, y_values_shifted,'.',color = 'dodgerblue',label=observatory)
+            plt.plot(x_shifted, y_values,'.',color = 'dodgerblue')   
+
+    plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator())
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+    plt.gcf().autofmt_xdate()  # Auto-format the date labels for better readability
+
+    #plt.tight_layout()  # Adjust layout for better spacing
+    #plt.title("%s trigger coverage on 2023-10-12" %(event_name))
+    plt.xlabel('Date [UTC]')
+    plt.ylabel('Total Covered Probability [%]')
+    #plt.xscale('log')  # Set X-axis to log scale
+    #plt.yscale('log')  # Set Y-axis to log scale
+    plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+
+    plt.legend()
+    plt.grid()
+    plt.savefig('%s_%s_cumulativeDistribution.png' %(event_name,WhatToPlot), dpi=300, bbox_inches='tight')
+    plt.close()
+    #plt.show()
+
+
+
+def ObtainCumulativeProbabilityPlotLST(folder_path, event_name, WhatToPlot):
+
+    # Get a list of all .txt files in the specified folder
+    
+    #file_list = glob.glob(folder_path)
+    #print(folder_path)
+
+    # Initialize an empty DataFrame to store the cumulative 5th columns
+    file_path =  folder_path 
+    cumulative_column = None
+
+    # Initialize a list to store individual 5th columns for plotting
+    individual_columns = []
+
+    # Loop through each file and add the 5th column to the cumulative DataFrame
+    # Loop through each file and add the 5th column to the cumulative DataFrame
+    # Read the .txt file into a Pandas DataFrame
+    df = pd.read_csv(file_path, sep=' ')  # Adjust the separator if needed
+
+    observatory_column = df.columns[-1]
+    df[WhatToPlot] = df[WhatToPlot]*100
+    grouped_data = df.groupby(observatory_column)
+
+    # Find the row with the maximum value in 'value_column' for each group in 'group_column'
+    df['cumsum'] = df[WhatToPlot].cumsum()  # Cumulative sum of PGal values
+
+    max_value_indices = df.groupby("Observation Time UTC")['cumsum'].idxmax()
+
+    # Select the rows with the maximum values
+    max_rows = df.loc[max_value_indices]
+
+    # Display the resulting DataFrame
+    print(max_rows)
+    print('---------------')
+    print('---------------')
+    print(df[WhatToPlot])
+    print('---------------')
+    print('---------------')
+    #x_values2 = pd.to_datetime(max_rows["Observation Time UTC"])
+
+    max_rows['date_column'] = pd.to_datetime(max_rows["Observation Time UTC"])+pd.Timedelta(minutes=15)
+
+    y_values2 = max_rows["cumsum"]
+    # Plot the cumulative values for all observatories
+    plt.plot(max_rows['date_column'], y_values2, '+',label='LST1-4', color='black')
+    
+
+    for observatory, observatory_data in grouped_data:
+        #print(f"Observatory: {observatory}")
+        #print(observatory_data)
+        print("\n")
+        # Extract relevant columns for the plot
+        x_values = pd.to_datetime(observatory_data["Observation Time UTC"])
+        #print(x_values)
+        y_values = observatory_data[WhatToPlot].cumsum()
+
+        # Plot the values for each observatory
+        y_values_shifted= [0] + y_values.tolist()[:-1]
+
+        # Remove the last element from the column
+        print(y_values)
+        print(y_values_shifted)
+        print(observatory)
+        if(observatory=='LST1'):
+            x_shifted=x_values+pd.Timedelta(minutes=15)
+            plt.plot(x_values, y_values_shifted,'.',color = 'darkorange', label=observatory)
+            plt.plot(x_shifted, y_values,'.', color = 'darkorange')
+        if(observatory=='LST2'):
+            x_shifted=x_values+pd.Timedelta(minutes=15)
+            plt.plot(x_values, y_values_shifted,'.',color = 'brown',label=observatory)
+            plt.plot(x_shifted, y_values,'.',color = 'brown')
+        if(observatory=='LST3'):
+            x_shifted=x_values+pd.Timedelta(minutes=15)
+            plt.plot(x_values, y_values_shifted,'.',color = 'blueviolet',label=observatory)
+            plt.plot(x_shifted, y_values,'.',color = 'blueviolet')    
+        if(observatory=='LST4'):
+            x_shifted=x_values+pd.Timedelta(minutes=15)
+            plt.plot(x_values, y_values_shifted,'.',color = 'yellowgreen',label=observatory)
+            plt.plot(x_shifted, y_values,'.',color = 'yellowgreen')   
+        #plt.plot([x_values[0], x_shifted[0]], [y_values_shifted[0], y_values[0]], color='red', linestyle='dashed', linewidth=2)
+
+
+
+    # Add labels and legend
+    # Format the x-axis to display date and time
+    # Set the locator and formatter for the x-axis to show dates
+    # Manually set date ticks on the x-axis
+    # Format the x-axis to display date and time
+    plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator())
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+    plt.gcf().autofmt_xdate()  # Auto-format the date labels for better readability
+
+    #plt.tight_layout()  # Adjust layout for better spacing
+    #plt.title("%s trigger coverage on 2023-10-12" %(event_name))
     plt.xlabel('Date [UTC]')
     plt.ylabel('Total Covered Probability [%]')
     #plt.xscale('log')  # Set X-axis to log scale
