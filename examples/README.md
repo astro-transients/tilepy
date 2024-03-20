7026493f060dc1f2517a3ba5cd3e34104bb70a61
Monica Seglar <monica.seglar-arroyo@cea.fr>
2024-03-20 19:29:20 +0100

    organizing the repo..
    Former-commit-id: 7b67ea3b122b793c48228655bc10813c4dbf67c6 [formerly 17f085eb4cc821da36e8637f0ebde15541642499]
    Former-commit-id: 8d4bf17838af1ae4b12660c5501077152a61a391
    Former-commit-id: 0280a4d8659e05866ab1b328908a1bd78a2e33f2

--- examples/README.md
+++ examples/README.md
@@ -1,9 +1,13 @@
 Using the examples provided here in this form (without edits) require: 
 
-a ./config folder containing the configuration files:   
-    ExampleConfig.ini for the regular 1-obs 
+a ./obsConfig folder containing configuration files:   
+    ExampleConfig.ini for regular 1-obs 
     FollowupParameters_[ObsName].ini for the N-obs scheduler.
 
-b. ./dataset folder containing the galaxy catalog "converted_GLADE.h5" (See main README for the creation of galaxy catalog)
+b. ./obsLauncher folder contains example scripts and jupyter notebooks to launch a scheduling
+
+c. ./obsCase folder contanins example of launcher scripts and output files of various cases (GW 1-obs, GRB N-obs, optical N-obs, IPN GRB 1-obs)
+
+Note that if yo ../dataset folder containing the galaxy catalog "converted_GLADE.h5" (See main README for the creation of galaxy catalog)
 
 Run the Jupyter notebook .ipynb or the .py scripts with Python > 3.9
---
+++ examples/obsCases/cumulative_plot.py
@@ -0,0 +1,13 @@
+from tilepy.tools.PlotCumulativeCoverage import ObtainCumulativeProbabilityPlotLST,ObtainCumulativeProbabilityPlotMWL,ObtainCumulativeProbabilityPlotCTA, ObtainCumulativeProbabilityPlotLog
+
+##ObtainCumulativeProbabilityPlot('S190814bv/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'Pgal')
+ObtainCumulativeProbabilityPlotLST('bn231012231_PAPER/PGWinFoV_NObs/SuggestedPointings_GWOptimisation.txt','GRB 231012A', 'PGW')
+##ObtainCumulativeProbabilityPlotLog('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation_noHA.txt','S190814bv', 'Pgal','2023-09-15 01:30:10')
+##ObtainCumulativeProbabilityPlotLog('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation_noHA.txt','S190814bv', 'PGW','2023-09-15 01:30:10')
+ObtainCumulativeProbabilityPlotMWL('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'Pgal')
+ObtainCumulativeProbabilityPlotMWL('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'PGW')
+
+##ObtainCumulativeProbabilityPlot('MS230826n_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S230826n', 'Pgal')
+##ObtainCumulativeProbabilityPlot('MS230826n_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S230826n', 'PGW')3
+ObtainCumulativeProbabilityPlotCTA('927563_lalinference_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','927563', 'PGW')
+ObtainCumulativeProbabilityPlotCTA('927563_lalinference_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','927563', 'Pgal')
