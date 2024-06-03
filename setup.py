7693979f59afa8dd6d043650c6e7d87e8aa6a6a1
Monica Seglar <monica.seglar-arroyo@cea.fr>
2024-06-03 20:06:25 +0200

    Updated in setup the version and other details

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readfile(filename):
 
 
 setup(name='tilepy',
-      version='1.7.1',
+      version='1.8.0',
       description='Computation of the tiling of large localization uncertainty region event with multi-wavelength pointing telescopes',
       install_requires=[
           'astropy', #>=4.1',
@@ -30,12 +30,11 @@ setup(name='tilepy',
       ],
       packages=find_packages(),
       # tests_require=['pytest'],
-      author='Monica Seglar-Arroyo ',
+      author='Monica Seglar-Arroyo, Halim Ashkar, Fabian Schussler, Mathieu de Bony ',
       author_email='mosear2@gmail.com ',
-      url='https://drf-gitlab.cea.fr/multimessenger-IRFU/cta/gw-follow-up-simulations',
+      url='https://github.com/astro-transients/tilepy',
       long_description=readfile('README.md'),
       include_package_data=True,
-      package_data={'': ['dataset/*.all']},
       # license='',
       classifiers=[],
       scripts=[],
