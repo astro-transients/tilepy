61ba4b78c9fc12d6a55e4814ce8cbe5c6ed3e098
Monica Seglar <mosear2@gmail.com>
2024-06-06 18:10:17 +0200

    Cumulative probability jupyter here included and renaming

---
+++ examples/obsCases/paperPlots/cumulative_plot.py
@@ -0,0 +1,12 @@
+from tilepy.tools.PlotCumulativeCoverage import ObtainCumulativeProbabilityPlot, ObtainCumulativeProbabilityPlotLog
+
+##ObtainCumulativeProbabilityPlot('S190814bv/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'Pgal')
+#ObtainCumulativeProbabilityPlot('bn231012231_PAPER/PGWinFoV_NObs/SuggestedPointings_GWOptimisation.txt','GRB 231012A', 'PGW')
+##ObtainCumulativeProbabilityPlotLog('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation_noHA.txt','S190814bv', 'Pgal','2023-09-15 01:30:10')
+##ObtainCumulativeProbabilityPlot('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation_noHA.txt','S190814bv', 'PGW','2023-09-15 01:30:10')
+#ObtainCumulativeProbabilityPlot('S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'Pgal')
+ObtainCumulativeProbabilityPlotLog(ObtainCumulativeProbabilityPlotLog('../S190814bv_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S190814bv', 'PGW')
+##ObtainCumulativeProbabilityPlot('MS230826n_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S230826n', 'Pgal')
+##ObtainCumulativeProbabilityPlot('MS230826n_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','S230826n', 'PGW')
+ObtainCumulativeProbabilityPlot('../927563_lalinference_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','927563', 'PGW')
+#ObtainCumulativeProbabilityPlotCTA('../927563_lalinference_PAPER/PGalinFoV_NObs/SuggestedPointings_GWOptimisation.txt','927563', 'Pgal')
