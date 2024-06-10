275ad850be544a50bd5621685fc639061854fbf6
Monica Seglar <mosear2@gmail.com>
2024-06-10 14:45:51 +0200

    Final version of CumulativePlot

--- tilepy/tools/PlotCumulativeCoverage.py
+++ tilepy/tools/PlotCumulativeCoverage.py
@@ -50,7 +50,7 @@ def ObtainCumulativeProbabilityPlot(folder_path, event_name, WhatToPlot, interac
             else:
                 plt.plot([xv, xs], [yvs, yv], marker='.', linestyle='-', color=colors[index])
         
-    plt.plot(max_rows['end_obs'], max_rows["cumsum"], marker='+', linestyle='-',label='CTAO full', color='black')
+    plt.plot(max_rows['end_obs'], max_rows["cumsum"], marker='+', linestyle='-',label='All', color='black')
     plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator())
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
     plt.gcf().autofmt_xdate()  # Auto-format the date labels for better readability
@@ -111,7 +111,7 @@ def ObtainCumulativeProbabilityPlotLog(folder_path, event_name, WhatToPlot, inte
             else:
                 plt.plot([xv, xs], [yvs, yv], marker='.', linestyle='-', color=colors[index])
 
-    plt.plot(max_rows['end_obs'], max_rows["cumsum"], marker='+', linestyle='-',label='All Observatories', color='black')
+    plt.plot(max_rows['end_obs'], max_rows["cumsum"], marker='+', linestyle='-',label='All', color='black')
     plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator())
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
     plt.gcf().autofmt_xdate()  # Auto-format the date labels for better readability
