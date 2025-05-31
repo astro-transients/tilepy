4b5a740d3c9d43f5f14a8259c122298eca3e37d2
Ramodgwendé Weizmann Kiendrébéogo <kiend.weizman7@gmail.com>
2025-05-31 15:18:29 +0200

    Add  sphinx configuration for readthedoc

--- docs/conf.py
+++ docs/conf.py
@@ -19,10 +19,27 @@ release = "1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+# The name for this set of Sphinx documents.  If None, it defaults to
+# "<project> v<release> documentation".
+# html_title = "TilePy"
+
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # Support for Google-style and NumPy-style docstrings
+    "sphinx.ext.intersphinx",  # Cross-references to the docs of other projects
+    "sphinx.ext.todo",  # Use .. todo:: directives in your docs
+    "sphinx.ext.viewcode",  # Add links to highlighted source code in the API docs
+    "sphinxcontrib.bibtex",  # Scientific references (for citations)
     # other extensions...
 ]
+
+# # Intersphinx mappings for cross-referencing other Python library documentation
+# intersphinx_mapping = {
+#     "numpy": ("https://numpy.org/doc/stable/", None),
+#     "astropy": ("https://docs.astropy.org/en/stable/", None),
+# }
+
+todo_include_todos = True  # Show TODO items in the built documentation
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -33,3 +50,13 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "alabaster"
 html_static_path = ["_static"]
+
+
+# Documentation site title (optional; defaults to "<project> v<release> documentation")
+html_title = "tilepy Documentation"
+
+# -- API documentation options -----------------------------------------------
+autodoc_typehints = "description"  # Show type hints in the API documentation (optional)
+
+# -- BibTeX for scientific references ----------------------------------------
+bibtex_bibfiles = ["refs.bib"]  # To bibliography
