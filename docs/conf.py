f5231f41215bd460ebc230b9206f835f177689d5
Ramodgwendé Weizmann Kiendrébéogo <kiend.weizman7@gmail.com>
2025-06-01 00:52:22 +0200

    Add configuration for htlm

--- docs/conf.py
+++ docs/conf.py
@@ -94,8 +94,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = "sphinx_rtd_theme"
-html_theme = "alabaster"
+# # html_theme = "sphinx_rtd_theme"
+# html_theme = "alabaster"
 html_static_path = ["_static"]
 html_logo = "_static/tilepy_logo.png"
 
@@ -107,10 +107,10 @@ htmlhelp_basename = project + "doc"
 # Prefixes that are ignored for sorting the Python module index
 modindex_common_prefix = ["tilepy."]
 
-# html_theme_options = {
-#     "github_url": "https://github.com/weizmannk/tilepy",
-#     "use_edit_page_button": True,
-# }
+html_theme_options = {
+    "github_url": "https://github.com/weizmannk/tilepy",
+    "use_edit_page_button": True,
+}
 
 
 html_context = {
