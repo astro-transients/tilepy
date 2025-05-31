1ec42cde6c1445212cb73f8de3f7083f183af39e
Ramodgwendé Weizmann Kiendrébéogo <kiend.weizman7@gmail.com>
2025-06-01 00:54:33 +0200

    Add configuration for htlm

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # # html_theme = "sphinx_rtd_theme"
-# html_theme = "alabaster"
+html_theme = "alabaster"
 html_static_path = ["_static"]
 html_logo = "_static/tilepy_logo.png"
 
