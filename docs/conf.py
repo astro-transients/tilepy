754ab055ece2d3f2fe956cc5d8926cca160b0fbc
Ramodgwendé Weizmann Kiendrébéogo <kiend.weizman7@gmail.com>
2025-06-01 00:47:55 +0200

    Add configuration for htlm

--- docs/conf.py
+++ docs/conf.py
@@ -94,11 +94,10 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 html_static_path = ["_static"]
-
 html_logo = "_static/tilepy_logo.png"
-
 
 # Documentation site title (optional; defaults to "<project> v<release> documentation")
 html_title = "TilePy"
