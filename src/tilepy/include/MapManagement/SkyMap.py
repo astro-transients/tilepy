392063e3c7eade55207c91601c160c4517c948ae
Alessio Berti <alessioberti90@gmail.com>
2026-03-22 18:08:19 +0100

    Use correct scheme and is_nested.

--- src/tilepy/include/MapManagement/SkyMap.py
+++ src/tilepy/include/MapManagement/SkyMap.py
@@ -104,7 +104,7 @@ class SkyMap:
             is3D = False
         return is3D
 
-    def getPixIdArea(self, fraction_localisation, nside=None, scheme="ring"):
+    def getPixIdArea(self, fraction_localisation, nside=None):
         """
         Return pixel indices covering a specified localization probability.
 
@@ -117,8 +117,6 @@ class SkyMap:
             Probability threshold (e.g., 0.9 for 90% localization).
         nside : int or None, optional
             HEALPix nside to use for rasterization (default: None).
-        scheme : str, optional
-             HEALPix ordering scheme, either 'ring' (default) or 'nested'.
 
         Returns
         -------
@@ -130,7 +128,7 @@ class SkyMap:
         cache_line = (
             f"{fraction_localisation}_raw"
             if nside is None
-            else f"{fraction_localisation}_{nside}_{scheme}"
+            else f"{fraction_localisation}_{nside}_{self.scheme}"
         )
         if cache_line in self.pix_id_area_cache.keys():
             return self.pix_id_area_cache[cache_line]
@@ -142,7 +140,7 @@ class SkyMap:
                 * self.raw_map_prob_density.pixarea(sorted_pixel_id)
             ).value
         else:
-            prob = self.getMap("prob", nside=nside, scheme=scheme)
+            prob = self.getMap("prob", nside=nside)
             sorted_pixel_id = np.flipud(np.argsort(prob))
             prob_sorted = prob[sorted_pixel_id]
         summed_probability = np.cumsum(prob_sorted)
@@ -155,7 +153,7 @@ class SkyMap:
 
     def getArea(self, fraction_localisation):
         area_vals = self.raw_map_prob_density.pixarea(
-            self.getPixIdArea(fraction_localisation)
+            self.getPixIdArea(fraction_localisation, None)
         )
         return np.sum(area_vals.to(u.deg * u.deg))
 
